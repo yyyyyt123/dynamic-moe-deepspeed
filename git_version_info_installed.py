3a25e3ec53f23158099aea5c81d549edebfafc06
Yang Yi Tao <ytyang@linux9.cse.cuhk.edu.hk>
2022-11-07 12:43:17 +0800

    original deepspeed

---
+++ git_version_info_installed.py
@@ -0,0 +1,6 @@
+version='0.7.4'
+git_hash='unknown'
+git_branch='unknown'
+installed_ops={'cpu_adam': False, 'cpu_adagrad': False, 'fused_adam': False, 'fused_lamb': False, 'sparse_attn': False, 'transformer': False, 'stochastic_transformer': False, 'async_io': False, 'utils': False, 'quantizer': False, 'transformer_inference': False}
+compatible_ops={'cpu_adam': True, 'cpu_adagrad': True, 'fused_adam': True, 'fused_lamb': True, 'sparse_attn': False, 'transformer': True, 'stochastic_transformer': True, 'async_io': True, 'utils': True, 'quantizer': True, 'transformer_inference': True}
+torch_info={'version': '1.8', 'bf16_support': False, 'cuda_version': '10.2', 'nccl_version': '2.7', 'hip_version': '0.0'}
